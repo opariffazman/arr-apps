1337b0c859330ce5e24337881eadf645b9340137
opariffazman <ariff.azman@proton.me>
2023-06-05 22:46:26 +0800

    feat: added jellyfin & jellyseer refactor: fixed and adapted to added feat

--- main.ps1
+++ main.ps1
@@ -12,24 +12,30 @@ function Add-Containarr {
   )
 
   $Ports = @{
-    "Prowlarr"  = "9696"
-    "Sonarr"    = "8989"
-    "Radarr"    = "7878"
-    "Bazarr"    = "6767"
-    "Overseerr" = "5055"
+    "Prowlarr"   = "9696"
+    "Sonarr"     = "8989"
+    "Radarr"     = "7878"
+    "Bazarr"     = "6767"
+    "Overseerr"  = "5055"
+    "Jellyseerr" = "5055"
   }
 
   $Port = $Ports[$name]
 
+  if ($name -eq 'Jellyseerr') {
+    $provider = 'fallenbagel'
+  } else {
+    $provider = 'lscr.io/linuxserver'
+  }
+
   $template = @"
   ${name}:
-    image: lscr.io/linuxserver/${name}:latest
     container_name: ${name}
+    image: ${provider}/${name}:latest
     environment:
       - TZ=`${TIMEZONE}
     volumes:
       - ./config/${name}-config:/config
-      - ./data:/data
     ports:
       - ${Port}:${Port}
     restart: unless-stopped
@@ -38,19 +44,43 @@ function Add-Containarr {
   
   $template | Out-File .\docker-compose.yml -Append
 }
+
 function Add-Plex {
   $template = @"
   plex:
-    image: lscr.io/linuxserver/plex:latest
     container_name: plex
+    image: lscr.io/linuxserver/plex:latest
     environment:
       - TZ=`${TIMEZONE}
       - VERSION=docker
     volumes:
       - ./config/plex-config:/config
-      - ./data/media/:/media
+      - `${DATAPATH}/media/:/media
     ports:
       - 32400:32400
+    restart: unless-stopped
+
+"@
+
+  $template | Out-File .\docker-compose.yml -Append
+}
+
+function Add-Jellyfin {
+  $template = @"
+  plex:
+    container_name: jellyfin
+    image: lscr.io/linuxserver/jellyfin:latest
+    environment:
+      - TZ=`${TIMEZONE}
+      - VERSION=docker
+    volumes:
+      - ./config/jellyfin-config:/config
+      - `${DATAPATH}/media/:/media
+    ports:
+      - 8096:8096
+      - 8920:8920 #optional
+      - 7359:7359/udp #optional
+      - 1900:1900/udp #optional
     restart: unless-stopped
 
 "@
@@ -61,16 +91,16 @@ function Add-Plex {
 function Add-Qbittorrent {
   $template = @"
   qbittorrent:
-    image: lscr.io/linuxserver/qbittorrent:latest
     container_name: qbittorrent
+    image: lscr.io/linuxserver/qbittorrent:latest
     environment:
       - TZ=`${TIMEZONE}
       - WEBUI_PORT=8080
     volumes:
       - ./config/qbittorrent-config:/config
-      - ./data/torrents:/data/torrents
-      - ./data/torrents/tv:/data/torrents/tv
-      - ./data/torrents/movies:/data/torrents/movies
+      - `${DATAPATH}/torrents:/data/torrents
+      - `${DATAPATH}/torrents/tv:/data/torrents/tv
+      - `${DATAPATH}/torrents/movies:/data/torrents/movies
     ports:
       - 8080:8080
       - 6881:6881
@@ -85,8 +115,8 @@ function Add-Qbittorrent {
 function Add-NginxProxyManager {
   $template = @"
   nginxproxymanager:
-    image: jc21/nginx-proxy-manager:latest
     container_name: nginxproxymanager
+    image: jc21/nginx-proxy-manager:latest
     environment:
       - TZ=`${TIMEZONE}
     ports:
@@ -96,8 +126,6 @@ function Add-NginxProxyManager {
     volumes:
       - ./config/nginxproxymanager-config/data:/data
       - ./config/nginxproxymanager-config/letsencrypt:/etc/letsencrypt
-    networks:
-      - wsl
     restart: unless-stopped
 
 "@
@@ -107,15 +135,13 @@ function Add-NginxProxyManager {
 
 function Add-DuckDns {
   $template = @"
-  app:
+  duckdns:
     container_name: duckdns
     image: lscr.io/linuxserver/duckdns:latest
     environment:
       - TZ=`${TIMEZONE}
       - SUBDOMAINS=`${DUCK_DNS_SUBDOMAINS}
       - TOKEN=`${DUCK_DNS_TOKEN}
-    networks:
-      - wsl
     restart: unless-stopped
 
 "@
@@ -128,7 +154,7 @@ function Add-DuckDns {
   "DUCK_DNS_TOKEN=${token}" | Out-File .\.env -Encoding utf8 -Append
 }
 
-function Initial {
+function Start-Yaml {
   $initial = @"
 ---
 services:
@@ -137,34 +163,12 @@ services:
   $initial | Out-File .\docker-compose.yml -Force
 }
 
-function Final {
-  $final = @"
-networks:
-  wsl:
-    external: true
-    driver: bridge
-"@
-
-  $final | Out-File .\docker-compose.yml -Append
-}
-
 try {
   $dockerVersion = docker --version
+  Set-Prompt -prompt "docker is installed. version: $dockerVersion, ensure docker daemon is running in the background & then press enter to proceed" -color green
 }
 catch {
-  $dockerVersion = $_.Exception.Message
-}
-
-if ($dockerVersion) {
-  Set-Prompt -prompt "docker is installed. version: $dockerVersion, ensure Docker Desktop is running in the background & then press enter to proceed" -color green
-}
-else {
-  Write-Host "docker desktop not installed, attempting to download & install" -ForegroundColor Yellow
-  if (-not(Test-Path "DockerDesktopInstaller.exe")) {
-    Invoke-WebRequest -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile DockerDesktopInstaller.exe
-  }
-  Start-Process "DockerDesktopInstaller.exe" -Wait install -Verbose
-  Set-Prompt -prompt "docker installation complete. press enter to exit & re-run the script after running docker desktop application manually" -color blue
+  Write-Host "docker is not installed, please download docker first" -ForegroundColor Yellow
 }
 
 $timeZone = Set-Prompt -prompt "timezone? [Default: Asia/Singapore]" -color magenta
@@ -173,17 +177,24 @@ if ($timeZone -eq '') {
 }
 "TIMEZONE=${timeZone}" | Out-File .\.env -Encoding utf8
 
-Initial
+$dataPath = Set-Prompt -prompt "data path? [Default: ./data]" -color magenta
+if ($dataPath -eq '') {
+  $dataPath = "./data"
+}
+"DATAPATH=${dataPath}" | Out-File .\.env -Encoding utf8 -Append
 
-$servarr = 'sonarr', 'radarr', 'bazarr', 'prowlarr', 'overseerr'
-$servarr | ForEach-Object {
-  $choice = Set-Prompt -prompt "install $($_)? [y/n]" -color magenta
-  if ($choice -eq 'y' -or $choice -eq 'Y' -or $choice -eq '') {
-    Add-Containarr -Name $_
-  }
+Start-Yaml
+
+$mediaServer = Set-Prompt -prompt "plex (overseerr) or jellyfin (jellyseerr)? [Default: plex]" -color magenta
+if ($mediaServer -eq '') {
+  $mediaServer = "plex"
+  $mediaTool = "overseerr"
+} else {
+  $mediaServer = "jellyfin"
+  $mediaTool = "jellyseerr"
 }
 
-$otherr = 'Plex', 'Qbittorrent', 'NginxProxyManager'
+$otherr = $mediaServer, 'Qbittorrent', 'NginxProxyManager', 'DuckDns'
 $otherr | ForEach-Object {
   $containerName = $($_).ToLower()
   $choice = Set-Prompt -prompt "install $($containerName)? [y/n]" -color magenta
@@ -192,7 +203,13 @@ $otherr | ForEach-Object {
   }
 }
 
-# Final
+$servarr = $mediaTool, 'sonarr', 'radarr', 'bazarr', 'prowlarr'
+$servarr | ForEach-Object {
+  $choice = Set-Prompt -prompt "install $($_)? [y/n]" -color magenta
+  if ($choice -eq 'y' -or $choice -eq 'Y' -or $choice -eq '') {
+    Add-Containarr -Name $_
+  }
+}
 
 Write-Host "a docker-compose.yml has been generated, you may check it first" -ForegroundColor Green
 $choice = Set-Prompt -prompt "Or straight away run `"docker compose up -d`"? [y/n]" -color magenta
@@ -201,9 +218,9 @@ if ($choice -eq 'y' -or $choice -eq 'Y' -or $choice -eq '') {
   docker compose up -d 
 }
 
-$folder = '.\data\media\tv', '.\data\media\movie'
+$folder = "${DATAPATH}\media\tv", "${DATAPATH}\media\movie"
 $folder | ForEach-Object { 
-  Write-Host "creating additional folder [${$_}]" -ForegroundColor blue
+  Write-Host "creating media folder [$_]" -ForegroundColor blue
   New-Item -ItemType Directory -Path $_ -ErrorAction SilentlyContinue | Out-Null
 }
 
